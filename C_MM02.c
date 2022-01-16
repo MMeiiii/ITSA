941137a073d125948cb476d6d2e8ae3a84184403
MMeiiii <77165926+MMeiiii@users.noreply.github.com>
2022-01-16 19:10:49 +0800

    Add files via upload

---
+++ C_MM02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+
+    float bottom, high; // ¤U©³ °ª
+
+    while(scanf("%f %f", &bottom, &high)!=EOF){
+
+
+        printf("%.1f\n",bottom*high/2); //©³*°ª/2
+
+    }
+
+    return 0;
+}
