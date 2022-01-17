e1df2ba95a99e5432f72bc0f6763339234d6a557
MMeiiii <77165926+MMeiiii@users.noreply.github.com>
2022-01-17 21:07:52 +0800

    Add files via upload

---
+++ C_MM26.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+
+   int x;
+
+    scanf("%d",&x);
+
+    for(int i=1;i<=x;i++){ //i*i=
+
+        printf("%d*%d=%d\n",i,i,i*i);
+
+    }
+
+    return 0;
+}
